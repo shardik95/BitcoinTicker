59dd86d7ff66920e721ed90463402c1f7a2b712f
shardik95 <shardik95@ccs.neu.edu>
2018-08-17 22:46:05 -0400

    Bitcoin Price tracker

---
+++ BitcoinTicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinTicker
+//
+//  Created by Hardik Shah on 8/10/18.
+//  Copyright © 2018 Hardik Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
